23b9ed7d460df3a2f94b8a018afa71ce662300ad
wjdgodud2617-pixel <wjdgodud2617@gmail.com>
2026-07-30 18:07:44 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='eldyn-project-v5-6-2-data-recovery';
+const CACHE='eldyn-project-v5-6-2-recovery';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./config.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
