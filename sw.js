359d16e0ac8381dfb86f6b2e5e66fd89eb69060e
wjdgodud2617-pixel <wjdgodud2617@gmail.com>
2026-08-05 10:47:52 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='eldyn-v1.2.9-stable-walk-crossfit-story';
+const CACHE='eldyn-v1.2.9.1-dashboard-ko';
 const RUN_NOTICE_TAG='eldyn-active-run';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./config.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
